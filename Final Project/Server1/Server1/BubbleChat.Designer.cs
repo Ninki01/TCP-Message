77bd33b1fd7a4dbe60e6a6ba6a67e1e071a5d264
Ninki01 <125526714+Ninki01@users.noreply.github.com>
2024-12-30 14:19:08 +0800

    Add files via upload

---
+++ Final Project/Server1/Server1/BubbleChat.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace Server1
+{
+    partial class BubbleChat
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Location = new System.Drawing.Point(13, 10);
+            this.lblMessage.MaximumSize = new System.Drawing.Size(300, 0);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(35, 13);
+            this.lblMessage.TabIndex = 0;
+            this.lblMessage.Text = "label1";
+            this.lblMessage.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // BubbleChat
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.Controls.Add(this.lblMessage);
+            this.Margin = new System.Windows.Forms.Padding(10);
+            this.Name = "BubbleChat";
+            this.Padding = new System.Windows.Forms.Padding(10);
+            this.Size = new System.Drawing.Size(169, 37);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.Label lblMessage;
+    }
+}
